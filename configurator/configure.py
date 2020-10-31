6fe979d522efe2410bcde4f3a92ab2839ea8ebdc
Abzicht <abzicht@gmail.com>
2020-10-31 12:25:27 +0100

    added configurator, increased sensor attempts and added configuration

--- configurator/configure.py
+++ configurator/configure.py
@@ -1,2 +1,28 @@
-def set_address(target):
-def set_offset(category):
+#!/usr/bin/python3
+
+from pymodbus.constants import Endian
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.payload import BinaryPayloadDecoder
+import sys
+
+def set_address(target:int, reg_addr=0x0100):
+    client = ModbusClient(method = 'rtu', port='/dev/ttyS0',stopbits = 1,timeout =0.3, bytesize = 8, parity = 'N', baudrate = 9600, driver_pin=36)
+    connection = client.connect()
+    if not connection:
+        print("Connection failed. Exiting", file=sys.stderr)
+        return 1
+    try:
+        if target:
+            print("Setting address to {}".format(target))
+            response = client.write_register(reg_addr,target,unit=0x0000)
+            print(response)
+    except Exception as e:
+        print(e, file=sys.stderr)
+    client.close()
+
+#def set_offset(category):
+if len(sys.argv)!= 2:
+    print("python3 configure.py TARGET_ADDRESS", file=sys.stderr)
+    sys.exit(1)
+target = int(sys.argv[1])
+sys.exit(set_address(target))
