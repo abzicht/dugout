25615d401001ac5eb35507bf3ed229e8dd8ec50f
Abzicht <abzicht@gmail.com>
2020-10-31 14:18:00 +0100

    working dockerfile

--- crawler/entrypoint.sh
+++ crawler/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ssh -L 127.0.0.01:$PORT:127.0.0.1:$PORT $SSHUSER@$SSHSERVER
-dugout-crawler $WORK/config.json -i 5 -v 3
+ssh -N -i $WORK/id_rsa -L 127.0.0.01:4321:127.0.0.1:4321 -o "ServerAliveInterval 120" -o "StrictHostKeyChecking no" $SSHUSER@$SSHSERVER &
+dugout-crawler $WORK/config.json -i 5 -v 2
