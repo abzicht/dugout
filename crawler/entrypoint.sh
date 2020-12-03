8554298d9f56f12649bfa7f9ea1cbbdd71618ce2
Abzicht <abzicht@gmail.com>
2020-12-03 19:43:35 +0100

    fixed entrypoint to redo ssh connections

--- crawler/entrypoint.sh
+++ crawler/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-ssh -N -i $WORK/id_rsa -L 127.0.0.01:4321:127.0.0.1:4321 -o "ServerAliveInterval 120" -o "StrictHostKeyChecking no" $SSHUSER@$SSHSERVER &
+
+function ssh_repeater () {
+	# rerun ssh as soon as the existing connection fails
+	while true
+	do
+		ssh -N -i $WORK/id_rsa -L 127.0.0.01:4321:127.0.0.1:4321 -o "ServerAliveInterval 120" -o "StrictHostKeyChecking no" $SSHUSER@$SSHSERVER;
+		echo "SSH connection failed. Establishing new connection."
+	done
+}
+
+ssh_repeater &
+
 dugout-crawler $WORK/config.json -i 5 -v 2
