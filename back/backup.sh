72feb0744a7ebe6de57bffdb4408fcc707e4f633
Abzicht <abzicht@gmail.com>
2020-12-27 17:33:35 +0100

    implemented backup module

--- back/backup.sh
+++ back/backup.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
 tmpback=/tmp/index_backup.json
-persistentback=/dugout-backup/data/index_backup.json
+persistentbacksensors=/dugout-backup/data/dugout_sensors_backup.json
+persistentbackweather=/dugout-backup/data/dugout_weather_backup.json
 elasticdump \
-  --input=$esurl/$esindex \
+  --input=http://elasticsearch:9200/dugout-sensors \
   --output=$tmpback \
   --type=data \
-  --httpAuthFile=/dugout-backup/auth.ini
+  --httpAuthFile=/dugout-backup/auth.txt
 
-mv $tmpback $persistentback
+mv $tmpback $persistentbacksensors
+
+# ----
+
+elasticdump \
+  --input=http://elasticsearch:9200/dugout-weather \
+  --output=$tmpback \
+  --type=data \
+  --httpAuthFile=/dugout-backup/auth.txt
+
+mv $tmpback $persistentbackweather
--- back/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "SHELL=/bin/sh
-* * * * * /dugout-backup/backup.sh
-" > /dugout-backup/scheduler.txt
-
-crontab /dugout-backup/scheduler.txt
-cron -f
